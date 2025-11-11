2771cd66a7bdabb0c4528d04549f1ef7d49da40e
Clase <julivalencia1124@gmail.com>
2025-11-11 09:16:29 -0500

    ll

--- python/HOME/basico.py
+++ python/HOME/basico.py
@@ -1,8 +1,5 @@
-Nombre_Usuario= input("Hola!! como te llamas?\n")
-print(f"Hola, {Nombre_Usuario} Bienvenido!!\n")
-Edad_Usuario= input("Cuantos años tienes?\n")
-edad=int(Edad_Usuario)
-if edad < 18:
-    print("jaja eres un bebe")
-else :
-    print("buejo hijueputa")
+
+class usuario:
+    def __init__(self):
+        self.nombre
+        pass
